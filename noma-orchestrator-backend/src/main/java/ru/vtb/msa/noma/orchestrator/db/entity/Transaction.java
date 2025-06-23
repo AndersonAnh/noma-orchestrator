7cb879e443aa1f6af8c7dcf2a6b44a9fedea1f49
Андрей <andrejsolov341@gmail.com>
2025-06-23 20:24:36 +0300

    Фикс бага с отображением списка транзакции

--- noma-orchestrator-backend/src/main/java/ru/vtb/msa/noma/orchestrator/db/entity/Transaction.java
+++ noma-orchestrator-backend/src/main/java/ru/vtb/msa/noma/orchestrator/db/entity/Transaction.java
@@ -1,21 +1,10 @@
 package ru.vtb.msa.noma.orchestrator.db.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.Version;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 import ru.vtb.msa.noma.orchestrator.enums.TransactionStatus;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -37,7 +26,7 @@ public class Transaction {
     @Column(name = "status")
     private TransactionStatus status;
     private String description;
-    private LocalDate timestamp;
+    private LocalDateTime timestamp;
 
     @Version
     private Integer version;
