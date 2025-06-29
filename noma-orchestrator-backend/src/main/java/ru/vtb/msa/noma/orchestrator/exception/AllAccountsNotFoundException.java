465a6c881c4486322b2c46bd49384c68d5a22cb4
Андрей <andrejsolov341@gmail.com>
2025-06-29 17:17:20 +0300

    Рефактор удалил ненужное исключение

--- noma-orchestrator-backend/src/main/java/ru/vtb/msa/noma/orchestrator/exception/AllAccountsNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.vtb.msa.noma.orchestrator.exception;
-
-public class AllAccountsNotFoundException extends RuntimeException {
-    public AllAccountsNotFoundException(String message) {
-        super(message);
-    }
-}
