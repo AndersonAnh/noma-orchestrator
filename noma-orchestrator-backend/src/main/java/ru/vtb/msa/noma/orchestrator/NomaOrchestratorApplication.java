49f42e003191b7fff9a90e74634a1a0c27cfe336
Андрей <andrejsolov341@gmail.com>
2026-03-10 22:28:12 +0700

    переделал проверку дедупликации на Redis

--- noma-orchestrator-backend/src/main/java/ru/vtb/msa/noma/orchestrator/NomaOrchestratorApplication.java
+++ noma-orchestrator-backend/src/main/java/ru/vtb/msa/noma/orchestrator/NomaOrchestratorApplication.java
@@ -3,7 +3,6 @@ package ru.vtb.msa.noma.orchestrator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cache.annotation.EnableCaching;
 
 
 @SpringBootApplication
